bd289b2f8555517120b62782b772aeab716dab41
Mate Hegyhati <hegyhati.work@gmail.com>
2017-08-18 20:12:34 +0200

    jobcreator extended

--- src/AppBundle/Command/NewJob.php
+++ src/AppBundle/Command/NewJob.php
@@ -20,40 +20,57 @@ class NewJob extends ContainerAwareCommand
         ->setName('app:new-job')
 
         // the short description shown while running "php bin/console list"
-        ->setDescription('Adds a new Job')
+        ->setDescription('Adds a new Job')        
 
         // the full command description shown when running the command with
         // the "--help" option
         ->setHelp('This command allows you to add a new Job to the database...')
+        
+        ->addArgument('testinput', InputArgument::OPTIONAL, 'Creating some built-in test jobs.')
     ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $job = new Job();
-        $helper = $this->getHelperSet()->get('question');
-
-        $job->setName($helper->ask($input, $output, 
-            new Question('<info>Name of the Job:</info> ', 'TestJob')));
-        
-        $job->setAuthor($helper->ask($input, $output, 
-            new Question('<info>Author of the Job:</info> ', 'TestJob')));
-        
-        $job->setInputFile($helper->ask($input, $output, 
-            new Question('<info>Input:</info> ', 'alma,korte,banan,barack')));
-        
-        $job->setFinalized($helper->ask($input, $output, 
-            new ConfirmationQuestion('<info>Is the job finalized?</info> ', false)));
-        
-        $job->setFinished($helper->ask($input, $output, 
-            new ConfirmationQuestion('<info>Is the job finished?</info> ', false)));
-        
         $em = $this->getContainer()->get('doctrine.orm.entity_manager');
         $repository = $em->getRepository("AppBundle:Job");
         
-        $em->persist($job);
-        $em->flush();
+        $test = $input->getArgument('testinput');
         
+        if (! $test ) {
+            $job = new Job();
+            $helper = $this->getHelperSet()->get('question');
+
+            $job->setName($helper->ask($input, $output, 
+                new Question('<info>Name of the Job:</info> ', 'TestJob')));
+            
+            $job->setAuthor($helper->ask($input, $output, 
+                new Question('<info>Author of the Job:</info> ', 'TestJob')));
+            
+            $job->setInputFile($helper->ask($input, $output, 
+                new Question('<info>Input:</info> ', 'alma,korte,banan,barack')));
+            
+            $job->setFinalized($helper->ask($input, $output, 
+                new ConfirmationQuestion('<info>Is the job finalized?</info> ', false)));
+            
+            $job->setFinished($helper->ask($input, $output, 
+                new ConfirmationQuestion('<info>Is the job finished?</info> ', false)));
+            $em->persist($job);
+        } else if ($test=="abc" || $test == "all") {
+            $jobabc = new Job();
+            $jobabc
+                ->setName("Character counter")
+                ->setAuthor("Anonymus")
+                ->setInputFile("adf,sdfg,gs,rg,sgfg,sgs,sfd,g,fdg,gerer,gsdfserg")
+                ->setFinalized(true)
+                ->setFinished(false)
+                ->setMapFunction("function map(string inputchunk){ var mapoutput = []; return mapoutput;}")
+                ->setReduceFunction("function reduce (string key, array values){ var reduceoutput = []; return reduceoutput;}")
+                ;
+            $em->persist($jobabc);
+        }
+        
+        $em->flush();        
     }
 }
 
