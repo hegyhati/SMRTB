aaa82d0ebfbeaa1f3b04695b80246ed8a445adc9
Mate Hegyhati <hegyhati.work@gmail.com>
2017-08-18 14:03:50 +0200

    First view and Twig templates

--- src/AppBundle/Controller/ListController.php
+++ src/AppBundle/Controller/ListController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace AppBundle\Controller;
+use AppBundle\Entity\Job;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-class ListController
+class ListController extends Controller
 {
     
     /**
@@ -12,7 +14,11 @@ class ListController
     */
     public function listAction()
     {
-        return new Response('<html><body>List of Jobs</body></html>');
+        $em=$this->getDoctrine()->getManager();
+		$repository=$this->getDoctrine()->getRepository('AppBundle:Job');
+		$jobs=$repository->findAll();
+		 
+		return $this->render('List.twig',array('jobs' => $jobs));
     }
 }
 
