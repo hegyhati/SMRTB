975a0ebf406cfafef3892cedd6a0690ea1a27253
Mate Hegyhati <hegyhati.work@gmail.com>
2017-08-20 19:17:50 +0200

    JOb solution automatic

--- web/js/complexcontribute.js
+++ web/js/complexcontribute.js
@@ -3,39 +3,38 @@ function contribute() {
     xhr.open("GET","/api/job/"+getJobId()+"/mapreduce", true);
     xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
     xhr.send("");
-    xhr.onloadend = function () {
-        console.log(xhr.response);
-        var data = JSON.parse(xhr.response);
-        if(data.state<2) {
-            console.log("Project not started, nothing to do");
-        } else if (data.state==2) {
-            console.log("Map job received");
-            var table = document.getElementById("mapjobtable");
-            var row = table.insertRow(-1);
-            row.id="map-"+data.mapjobid;
-            row.insertCell(0).innerHTML=data.mapjobid;
-            row.insertCell(1).innerHTML=data.chunk;
-            row.insertCell(2).innerHTML="in progress...";
-            mapsolve(data);
-        } else if (data.state==3) {
-            console.log("Shuffling in progress, let's wait a little");
-        } else if (data.state==4) {
-            console.log("Reduce job received");
-            var table = document.getElementById("reducejobtable");
-            var row = table.insertRow(-1);
-            row.id="reduce-"+data.reducejobid;
-            row.insertCell(0).innerHTML=data.reducejobid;
-            row.insertCell(1).innerHTML=data.reducekey;
-            row.insertCell(2).innerHTML=data.values;
-            row.insertCell(3).innerHTML="in progress...";
-            reducesolve(data);
-        } else if(data.state==5) {
-            console.log("Job finished, redirect to statistics");
-        }
-    };
+    xhr.onloadend = function (){interpretjob(JSON.parse(xhr.response)); };
 }
 
-
+function interpretjob(data) {
+    console.log(data);
+    if(data.state<2) {
+        console.log("Project not started, nothing to do");
+    } else if (data.state==2) {
+        console.log("Map job received");
+        var table = document.getElementById("mapjobtable");
+        var row = table.insertRow(-1);
+        row.id="map-"+data.mapjobid;
+        row.insertCell(0).innerHTML=data.mapjobid;
+        row.insertCell(1).innerHTML=data.chunk;
+        row.insertCell(2).innerHTML="in progress...";
+        mapsolve(data);
+    } else if (data.state==3) {
+        console.log("Shuffling in progress, let's wait a little");
+    } else if (data.state==4) {
+        console.log("Reduce job received");
+        var table = document.getElementById("reducejobtable");
+        var row = table.insertRow(-1);
+        row.id="reduce-"+data.reducejobid;
+        row.insertCell(0).innerHTML=data.reducejobid;
+        row.insertCell(1).innerHTML=data.reducekey;
+        row.insertCell(2).innerHTML=data.values;
+        row.insertCell(3).innerHTML="in progress...";
+        reducesolve(data);
+    } else if(data.state==5) {
+        console.log("Job finished, redirect to statistics");
+    }
+}
 
 function mapsolve(data) {    
     var results=map(data.chunk);
@@ -52,9 +51,7 @@ function mapsolve(data) {
     xhr.open("POST","/api/mapresult/"+json.jobid+"/"+json.mapjobid, true);
     xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
     xhr.send(JSON.stringify(json));
-    xhr.onloadend = function () {
-        console.log(xhr.response);
-    };
+    xhr.onloadend = function (){interpretjob(JSON.parse(xhr.response));};
 }
 
 function pairdecoder(pairs){
@@ -80,8 +77,6 @@ function reducesolve(data) {
     xhr.open("POST","/api/reduceresult/"+json.jobid+"/"+json.reducejobid, true);
     xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
     xhr.send(JSON.stringify(json));
-    xhr.onloadend = function () {
-        console.log(xhr.response);
-    };    
+    xhr.onloadend = function (){interpretjob(JSON.parse(xhr.response));};
 }
     
