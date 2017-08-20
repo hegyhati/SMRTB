3eb56ac0c45a93482a59c22a31c07390afdceaa9
Mate Hegyhati <hegyhati.work@gmail.com>
2017-08-20 16:37:15 +0200

    Project edit function

--- src/AppBundle/Controller/ComplexController.php
+++ src/AppBundle/Controller/ComplexController.php
@@ -16,19 +16,55 @@ class ComplexController extends Controller
 {
     
     /**
-    * @Route("/complex/{jobid}/{work}", name="complex", defaults={"jobid"= null, "work"=false})
+    * @Route("/complex/{jobid}/{workerstate}", name="complex", defaults={"jobid"= null, "workerstate"=null})
     * @Method("GET")
     */
-    public function complexAction($jobid,$work)
+    public function complexAction($jobid,$workerstate)
     {
         $em=$this->getDoctrine()->getManager();
 		$repository=$this->getDoctrine()->getRepository('AppBundle:Job');
 		$job=$repository->findOneById($jobid);
         $jobs=$repository->findAll();
 		 
-		return $this->render('Complex.twig', array('jobs'=> $jobs, 'job' => $job, 'work' => $work));
+		return $this->render('Complex.twig', array('jobs'=> $jobs, 'job' => $job, 'workerstate' => $workerstate));
     }
     
+    /**
+    * @Route("/complex/new/", name="complexNewJob")
+    * @Method("GET")
+    */
+    public function complexNewJobAction()
+    {
+        $em=$this->getDoctrine()->getManager();
+		$repository=$this->getDoctrine()->getRepository('AppBundle:Job');
+        $job = new Job();
+        $job -> setName("New Project");
+        $em->persist($job);
+        $em->flush();        
+		 
+		return $this->redirectToRoute('complex', array('jobid' => $job->getId(), 'workerstate'=>'edit'));
+    }
+    
+    /**
+    * @Route("/complex/{jobid}/save", name="complexSaveJob")
+    * @Method("POST")
+    */
+    public function complexSaveJobAction($jobid)
+    {
+        $em=$this->getDoctrine()->getManager();
+		$repository=$this->getDoctrine()->getRepository('AppBundle:Job');
+        $request = Request::createFromGlobals();
+        
+        $job = $repository->findOneById($jobid);
+        $job 
+            -> setName($request->request->get('name'))
+            -> setMapFunction($request->request->get('mapfunction'))
+            -> setReduceFunction($request->request->get('reducefunction'))
+            -> setInputFile($request->request->get('input'));
+        $em->flush();        
+		 
+		return $this->redirectToRoute('complex', array('jobid' => $jobid, 'workerstate'=>'edit'));
+    }
 
    
 }
